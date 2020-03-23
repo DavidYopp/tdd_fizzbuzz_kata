a8b0916bd4bff343cedbc7173428fda1d0555ca3
Josh Yopp <davidjyopp@gmail.com>
2020-03-23 14:05:43 -0400

    added a docstring and comment to FizzBuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,15 @@
 
-
 def process_num(number):
+    """
+    Processes numbers and if they are divisible by 3 then it returns 'Fizz'
+    if they are divisible by 5 returns 'Buzz'
+    if they are divisible by 3 and 5 it return 'FizzBuzz'
+    other numbers just return themselves
+
+    Keywords args
+    number: integer -- number to process
+    returns: string or integer
+    """
 
     if number % 3 == 0 and number % 5 == 0:
         return ("FizzBuzz")
@@ -16,5 +25,6 @@ def process_num(number):
 
 
 if __name__ == '__main__':
+    #Processes the numbers 1 - 100 using the process_num function
     for i in range(1, 101):
         print(process_num(i))
