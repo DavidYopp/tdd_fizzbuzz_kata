1656dd9829d2e179bd43a8e6f78e4cec28be84c9
Josh Yopp <davidjyopp@gmail.com>
2020-03-23 13:58:02 -0400

    adding the test file and the fizzbuzz.py file

---
+++ fizzbuzz.py
@@ -0,0 +1,20 @@
+
+
+def process_num(number):
+
+    if number % 3 == 0 and number % 5 == 0:
+        return ("FizzBuzz")
+
+    elif number % 5 == 0:
+        return ("Buzz")
+
+    elif number % 3 == 0:
+        return ("Fizz")
+
+    else:
+        return number
+
+
+if __name__ == '__main__':
+    for i in range(1, 101):
+        print(process_num(i))
---
+++ test_fizzbuzz.py
@@ -0,0 +1,27 @@
+import unittest
+import fizzbuzz
+
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def test_multiple_of_3_returns_fizz(self):
+        result = fizzbuzz.process_num(3)
+        self.assertEqual("Fizz", result)
+
+
+    def test_multiple_of_5_returns_buzz(self):
+        result = fizzbuzz.process_num(5)
+        self.assertEqual('Buzz', result)
+
+
+    def test_multiple_of_3_and_5_returns_fizzbuzz(self):
+        result = fizzbuzz.process_num(15)
+        self.assertEqual('FizzBuzz', result)
+
+    def test_if_other_numbers_return_themselves(self):
+        result = fizzbuzz.process_num(2)
+        self.assertEqual(2, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
